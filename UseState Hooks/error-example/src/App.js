9926b49c6b894fad461a964fd78785a7a6a77c0e
Prince Rathor <60307000+prince-developer-csharp@users.noreply.github.com>
2021-05-20 12:21:13 +0530

    'thisexamplegiveideawhyusinguseStatehooks'

--- UseState Hooks/error-example/src/App.js
+++ UseState Hooks/error-example/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+
+  let title = 'Random Title';
+
+  const handleClick = () => {
+    title = 'Hello World';
+    console.log(title);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button onClick = {handleClick}>Change Title</button>
+    </React.Fragment>
   );
 }
 
